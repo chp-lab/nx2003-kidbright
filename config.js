97ca9d3a5741b11e78f4f49c791d0c3dce127a56
woraponks <worapon.ks@gmail.com>
2020-06-30 17:36:20 +0700

    edit name

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {    
-    name : 'nx2003-kidbright',
+    name : 'nx2003-wrover',
     platform : 'arduino-esp32',
     title : 'NX2003 With ESP32 Wrover-IB',
     description : '16MB Flash 4 MB PSRAM.\n',
