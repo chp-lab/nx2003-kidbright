2e95a6ea8de5ca49755513fcad632d8a16439ef0
woraponks <worapon.ks@gmail.com>
2020-07-16 10:10:49 +0700

    update

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     name : 'nx2003-wrover',
     platform : 'arduino-esp32',
-    title : 'NX2003 With ESP32 Wrover-IB',
-    description : '16MB Flash 4 MB PSRAM.\n',
+    title : 'NX2003 WROWER-B',
+    description : 'NEXPIE 16MB Flash 8 MB PSRAM.\n',
     author : 'Chatpeth Kenanan',
     website : 'https://www.espressif.com/sites/default/files/documentation/esp32-wrover-b_datasheet_en.pdf',
     email : 'ch.ee.psu@outlook.com',
     git : 'https://github.com/chp-lab/nx2003-kidbright/',
-    image : '/static/board_NX2003.jpg',
-    version : '1.0.0'
+    image : '/static/FRONT BOARD.png',
+    version : '1.1.0'
 };
