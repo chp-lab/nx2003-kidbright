2835d9bcb5cdf0612a36fae7b6101743212d977a
chatpeth.ke <chatpeth.ke@inet.co.th>
2020-07-17 09:39:32 +0700

    db0x02

---
+++ build/b4-e6-2d-e0-5c-5d/user_app.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+#include <WebServer.h>
+
+
+
+
+
+
+
+
+void setup()
+{
+  
+  music.song((std::vector<int>{262,494,330}),100);
+}
+void loop()
+{
+  
+  
+}
